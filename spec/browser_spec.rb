ce7f2b5268fecf467b50b94b51a112ea2ac21810
William Estoque <william.estoque@gmail.com>
2012-06-08 15:35:52 -0700

    Added the test server. Defaulted rake to run spec.

--- spec/browser_spec.rb
+++ spec/browser_spec.rb
@@ -1,5 +1,7 @@
 require 'phantom_menace'
 
+TEST_URL = "http://localhost:4567/index.html"
+
 describe PhantomMenace::Browser do
   before do
     @browser = PhantomMenace::Browser.new
@@ -7,16 +9,28 @@ describe PhantomMenace::Browser do
 
   describe "#goto" do
     it "responds with success if page opens successfully" do
-      resp = @browser.goto "http://google.com"
+      resp = @browser.goto TEST_URL
       resp["success"].should == true
     end
   end
 
   describe "#find_all_links" do
     it "responds with the links in the page" do
-      @browser.goto "http://google.com"
+      @browser.goto TEST_URL
       resp = @browser.find_all_links
       resp.length.should_not be(0)
+    end
+  end
+
+  describe "#find" do
+    it "responds with the links in the page" do
+      pending "Still working on it"
+    end
+  end
+
+  describe "#render" do
+    it "saves the file in the path specified" do
+      pending "Still working on it"
     end
   end
 end
---
+++ spec/test_server/README.md
@@ -0,0 +1,10 @@
+# Test Server
+
+The test server is a sinatra based server that serves static files
+needed by the tests.
+
+## Running
+
+```sh
+bundle exec ruby server.rb
+```
